e00c04d49d6c61ebccf5e990f39cae65aec8de75
Anke Dubois <duboisanke@gmail.com>
2022-07-25 11:28:22 +0200

    Initial commit

---
+++ CurriculumVitaeAnke/Controllers/PortfolioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CurriculumVitaeAnke.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CurriculumVitaeAnke/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CurriculumVitaeAnke.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
